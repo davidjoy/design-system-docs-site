dea2bae970606623a401acc197b96ee6874e726f
David Joy <djoy@vistahigherlearning.com>
2025-09-24 14:34:20 -0400

    fix: one last try at fixing the router

--- src/router.ts
+++ src/router.ts
@@ -1,20 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DesignSystemDocumentation from './design-system-documentation.mdx'
+import ComponentsPage from './components-page.mdx'
+import Utilities from './utilities.mdx'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('./design-system-documentation.mdx')
+    component: DesignSystemDocumentation
   },
   {
     path: '/components-page',
     name: 'Components',
-    component: () => import('./components-page.mdx')
+    component: ComponentsPage
   },
   {
     path: '/utilities',
     name: 'Utilities', 
-    component: () => import('./utilities.mdx')
+    component: Utilities
   }
 ]
 
